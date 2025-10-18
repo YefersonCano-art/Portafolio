2b8bb4fd86213f2883e298ae042787e0d2b5adc8
Jefferson David Cano Hernandez <71351421+YefersonD-art@users.noreply.github.com>
2025-10-17 20:36:24 -0500

    se crea el proyecto y se inicia la seccion hero

---
+++ src/components/icons/index.ts
@@ -0,0 +1,4 @@
+
+export { default as GitHubIcon } from './GitHubIcon.astro';
+export { default as LinkedInIcon } from './LinkedInIcon.astro';
+export { default as MailIcon } from './MailIcon.astro';
